6be9af476cf117b891d3095c087531f137d082e1
S I M O N. P <spau@hotmail.de>
2023-05-18 21:40:28 +0200

    working curve fitting

--- src/utils/Debugging/CurveFitting.cpp
+++ src/utils/Debugging/CurveFitting.cpp
@@ -7,16 +7,19 @@ namespace ViscoCorrect
     {
         CurveFitting::CurveFitting()
         {
-            //populate x and y Data
-            for(const auto& pair : data)
+            // populate x and y Data
+            for (const auto &pair : data)
             {
                 xData.push_back(pair.first);
                 yData.push_back(pair.second);
             }
+            compressed_data.dataSize = xData.size();
+            compressed_data.xData = xData.data();
+            compressed_data.yData = yData.data();
 
-            PlotRender_func = std::make_shared<std::function<void()>>(std::bind(RenderInPlot, this)); 
+            PlotRender_func = std::make_shared<std::function<void()>>(std::bind(RenderInPlot, this));
         }
-        
+
         CurveFitting::~CurveFitting()
         {
         }
@@ -24,21 +27,108 @@ namespace ViscoCorrect
         void CurveFitting::Render()
         {
             ImGui::Begin("Curve Fitting");
-            if(ImGui::Button("Enable Render"))
+            if (ImGui::Button("Enable Render") && !b_renderplot)
             {
                 Application::GetInstance()->GetGraph()->AddCallbackToPlot(PlotRender_func, 1);
+                b_renderplot = true;
             }
-            if(ImGui::Button("Disable Render"))
+            ImGui::SameLine();
+            if (ImGui::Button("Disable Render") && b_renderplot)
             {
                 Application::GetInstance()->GetGraph()->RemoveCallbackFromPlot(PlotRender_func, 1);
+                b_renderplot = false;
             }
+
+            if (ImGui::Button("Approximate Curve"))
+            {
+                FitCurve();
+                for(int i = xData.at(0); i < xData.back(); i++)
+                {
+                    fittedX.push_back(i);
+                    fittedY.push_back(a * i * i + b * i + c);
+                }
+                b_function = true;
+            }
+
             ImGui::End();
         }
 
         void CurveFitting::RenderInPlot()
         {
             ImPlot::PlotScatter("CQ", xData.data(), yData.data(), xData.size());
+            ImPlot::PlotLine("fit", fittedX.data(), fittedY.data(), fittedX.size());
+        }
+
+        // Curve Fitting
+
+
+        int CurveFitting::ResidualFunction(const gsl_vector *x, void *params, gsl_vector *f)
+        {
+            ComprestCurveData *_data = static_cast<ComprestCurveData *>(params);
+
+            for (size_t i = 0; i < _data->dataSize; i++)
+            {
+                double _yModel = _data->ModelFunction(_data->xData[i], x);
+                gsl_vector_set(f, i, _data->yData[i] - _yModel);
+            }
+
+            return GSL_SUCCESS;
+        }
+
+        void CurveFitting::FitCurve()
+        {
+            size_t _datasize = xData.size();
+
+            gsl_vector *xVec = gsl_vector_alloc(_datasize);
+            gsl_vector *yVec = gsl_vector_alloc(_datasize);
+
+            for (size_t i = 0; i < _datasize; i++)
+            {
+                gsl_vector_set(xVec, i, xData.at(i));
+                gsl_vector_set(yVec, i, yData.at(i));
+            }
+
+            gsl_vector *_initial_params = gsl_vector_alloc(num_parameters);
+
+            gsl_vector_set(_initial_params, 0, 1.0);
+            gsl_vector_set(_initial_params, 1, 1.0);
+            gsl_vector_set(_initial_params, 2, 1.0);
+
+            gsl_multifit_nlinear_parameters fittingParams = gsl_multifit_nlinear_default_parameters();
+            gsl_multifit_nlinear_workspace *workspace = gsl_multifit_nlinear_alloc(gsl_multifit_nlinear_trust, &fittingParams, _datasize, num_parameters);
+
+            gsl_multifit_nlinear_fdf fittingFunction;
+            fittingFunction.f = &ResidualFunction;
+            fittingFunction.df = nullptr;  // Set to nullptr for numerical derivatives
+            fittingFunction.fvv = nullptr; // Set to nullptr for numerical derivatives
+            fittingFunction.n = _datasize;
+            fittingFunction.p = num_parameters;
+            fittingFunction.params = static_cast<void *>(&compressed_data);
+
+            int info;
+
+            gsl_multifit_nlinear_init(_initial_params, &fittingFunction, workspace);
+
+            if (gsl_multifit_nlinear_driver(100, 1e-6, 1e-6, 1e-6, nullptr, nullptr, &info, workspace) != GSL_SUCCESS)
+            {
+                gsl_vector_free(xVec);
+                gsl_vector_free(yVec);
+                gsl_vector_free(_initial_params);
+                gsl_multifit_nlinear_free(workspace);
+                return;
+            }
+
+            // Retrieve the optimized parameter values
+            gsl_vector *solution = gsl_multifit_nlinear_position(workspace);
+            a = gsl_vector_get(solution, 0);
+            b = gsl_vector_get(solution, 1);
+            c = gsl_vector_get(solution, 2);
+
+            gsl_vector_free(xVec);
+            gsl_vector_free(yVec);
+            gsl_vector_free(_initial_params);
+            gsl_multifit_nlinear_free(workspace);
         }
     } // namespace Debug
-    
+
 } // namespace ViscoCorrect
--- src/utils/Debugging/CurveFitting.h
+++ src/utils/Debugging/CurveFitting.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "imgui.h"
-#include "implot.h" 
+#include "implot.h"
 
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_multifit_nlinear.h>
+#include <gsl/gsl_blas.h>
+
 #include <vector>
 #include <map>
 #include <functional>
@@ -14,6 +16,21 @@ namespace ViscoCorrect
 {
     namespace Debug
     {
+        struct ComprestCurveData
+        {
+            size_t dataSize;
+            double *xData;
+            double *yData;
+            double ModelFunction(double _x, const gsl_vector *_parameters)
+            {
+                double a = gsl_vector_get(_parameters, 0);
+                double b = gsl_vector_get(_parameters, 1);
+                double c = gsl_vector_get(_parameters, 2);
+
+                // Evaluate the model function for the given x-value
+                return a * _x * _x + b * _x + c;
+            };
+        };
         class CurveFitting
         {
         public:
@@ -24,8 +41,7 @@ namespace ViscoCorrect
             void FitCurve();
 
         private:
-            double ModelFunction(double, const gsl_vector*);
-            int ResidualFunction(const gsl_vector*, void *, gsl_vector*);
+            static int ResidualFunction(const gsl_vector *, void *, gsl_vector *);
 
             void RenderInPlot();
 
@@ -34,34 +50,41 @@ namespace ViscoCorrect
 
             std::vector<double> xData;
             std::vector<double> yData;
-            double a,b,c;
+            std::vector<double> fittedX;
+            std::vector<double> fittedY;
+
+            ComprestCurveData compressed_data;
+            size_t num_parameters = 3;
+            double a, b, c;
+
+
+            bool b_renderplot = false, b_function = false;
 
             std::shared_ptr<std::function<void()>> PlotRender_func;
 
             std::map<int, int> data{
-                {242,174},
-                {242,173},
-                {246,173},
-                {253,173},
-                {255,172},
-                {262,172},
-                {262,171},
-                {263,171},
-                {268,171},
-                {272,169},
-                {276,169},
-                {277,168},
-                {287,164},
-                {288,164},
-                {292,162},
-                {302,157},
-                {314,152},
-                {322,145},
-                {328,140},
-                {334,136}
-            };
+                {242, 174},
+                {242, 173},
+                {246, 173},
+                {253, 173},
+                {255, 172},
+                {262, 172},
+                {262, 171},
+                {263, 171},
+                {268, 171},
+                {272, 169},
+                {276, 169},
+                {277, 168},
+                {287, 164},
+                {288, 164},
+                {292, 162},
+                {302, 157},
+                {314, 152},
+                {322, 145},
+                {328, 140},
+                {334, 136}};
         };
-        
+
     } // namespace Debug
-    
+
 } // namespace ViscoCorrect
