74a15f01138cc6c6bfdff39ce19f7eae2893b8cd
S I M O N. P <spau@hotmail.de>
2023-08-29 21:08:00 +0200

    Minor improvements to build system

--- cmake/target_debug_tools.cmake
+++ cmake/target_debug_tools.cmake
@@ -1,12 +1,14 @@
 cmake_minimum_required(VERSION 3.5.1)
 
+
 set (BUILDLIBS "multifit_nlinear" CACHE STRING "")
 set (NO_AMPL_BINDINGS ON CACHE BOOL "")
 set (GSL_DISABLE_TESTS ON CACHE BOOL "")
+set (DOCUMENTATION OFF CACHE BOOL "")
 
 add_subdirectory(external/gsl)
 
-add_library(debug_tools
+add_library(debug_tools 
 ${_visco_debug_srcs}
 )
 
@@ -14,7 +16,9 @@ target_include_directories(debug_tools PUBLIC
     ${_visco_debug_includes}
 )
 
-add_dependencies(debug_tools gsl ${_visco_TARGET_LIBRARIES})
+add_dependencies(debug_tools gsl gslcblas ${_visco_TARGET_LIBRARIES})
 target_compile_definitions(debug_tools PUBLIC DEBUG_BUILD)
 
-target_link_libraries(debug_tools PUBLIC imgui implot gsl gslcblas)
+target_link_libraries(debug_tools PRIVATE imgui implot gsl gslcblas)
+
+add_dependencies(ViscoCorrect debug_tools)
